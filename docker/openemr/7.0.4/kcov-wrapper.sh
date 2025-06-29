bc3894deade5cde01f5e3a94813961bd669e3849
Brady Miller <brady.g.miller@gmail.com>
2025-06-29 03:35:37 -0700

    fixes

--- docker/openemr/7.0.4/kcov-wrapper.sh
+++ docker/openemr/7.0.4/kcov-wrapper.sh
@@ -6,9 +6,9 @@ set -e
 mkdir -p /var/www/localhost/htdocs/coverage
 
 # Run openemr.sh under kcov
-kcov --include-path=/var/www/localhost/htdocs/openemr.sh,/root/devtoolsLibrary.source \
+kcov --include-path=/var/www/localhost/htdocs/openemr/openemr.sh,/root/devtoolsLibrary.source \
      /var/www/localhost/htdocs/coverage \
-     /var/www/localhost/htdocs/openemr.sh
+     /var/www/localhost/htdocs/openemr/openemr.sh
 
 # When kcov is done, the script will have been executed
 # and we're ready to serve requests
